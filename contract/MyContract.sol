c8eda66e8a0cd5d7d2c97b99367d57770e2466c9
filoxtom <filoxtoma@gmail.com>
2024-07-02 18:06:32 +0200

    state vs local

--- contract/MyContract.sol
+++ contract/MyContract.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
-    // creating variables
+    // state variables = 
+    uint myUint = 1; // stored on the blockchain, can be accessed in the entire smart contract
+
+    // local variables = exist inside a function
+    function getValue() public pure returns(uint) {
+        uint value = 1; // local variable, readable/writable only here
+        return value;
+    }
+
+
+    
 }
